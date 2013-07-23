8eb82211234b372dc5384e625bc76b7500d0a3e0
wes <takachi0322@gmail.com>
2013-07-23 15:20:25 +0800

    add check -> when Xcode build, select "append" option, than not append framework again

--- unity4.0/Assets/Editor/SoomlaRunner.py
+++ unity4.0/Assets/Editor/SoomlaRunner.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 
-import sys, os.path
-import shutil
-
+import sys
+import build_xcode_config
 from mod_pbxproj import XcodeProject
+
 projectPath = sys.argv[1]
-projectPath = projectPath.replace('_;@#',' ')
+projectPath = projectPath.replace('_;@#', ' ')
 
-project = XcodeProject.Load(projectPath + '/Unity-iPhone.xcodeproj/project.pbxproj')
+if not build_xcode_config.is_append_mode(projectPath, 'IAP_SETTING_STATE', 'is_iap_set'):
+    project = XcodeProject.Load(
+        projectPath + '/Unity-iPhone.xcodeproj/project.pbxproj')
+    project.add_file(
+        'System/Library/Frameworks/Security.framework', tree='SDKROOT')
+    project.add_file('usr/lib/libsqlite3.0.dylib', tree='SDKROOT')
+    project.add_file(
+        'System/Library/Frameworks/StoreKit.framework', tree='SDKROOT')
+    project.add_other_ldflags('-ObjC')
 
-project.add_file('System/Library/Frameworks/Security.framework', tree='SDKROOT')
-project.add_file('usr/lib/libsqlite3.0.dylib', tree='SDKROOT')
-project.add_file('System/Library/Frameworks/StoreKit.framework', tree='SDKROOT')
-project.add_other_ldflags('-ObjC')
-
-project.saveFormat3_2()
+    project.saveFormat3_2()
